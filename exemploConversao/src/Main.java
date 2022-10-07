d8b7f58e1ed31cc42ee014a1229c17bf900a5acc
Rogatto <105252439+LucasRogatto21@users.noreply.github.com>
2022-10-07 19:14:51 -0300

    Add files via upload

---
+++ exemploConversao/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 1;
+
+        String nome = "Marcos";
+        String valor = "10";
+
+        System.out.println(Integer.parseInt(valor) + x);
+
+        System.out.println(valor + Integer.toString(x));
+
+    }   
+}
